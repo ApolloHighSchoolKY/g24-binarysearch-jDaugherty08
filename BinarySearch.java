017c2a76904642fbf8ea5f58e6fa6a7915307913
jDaugherty08 <joseph.daugherty@stu.daviess.kyschools.us>
2025-04-17 02:25:42 +0000

    i did it i think

--- BinarySearch.java
+++ BinarySearch.java
@@ -2,8 +2,32 @@ import java.util.Arrays;
 
 public class BinarySearch
 {
+	//remember bottom top and middle
 	public static int binarySearch(int[] ray, int item)
 	{
+		Arrays.sort(ray);
 
+		int bottom = 0;
+		int middle = 0;
+		int top = ray.length;
+
+		while(bottom <= top)
+		{
+			middle = (bottom+top)/2;
+			//too high
+			if(ray[middle] > item)
+				top = middle - 1;
+			
+			//too low
+			else if(ray[middle] < item)
+				bottom = middle + 1;
+			//else if(bottom > top && ray[middle] != item)
+				//return -1;
+			else if(ray[middle] == item)
+				return middle;
+			
+		}
+
+		return -1;
 	}
 }
